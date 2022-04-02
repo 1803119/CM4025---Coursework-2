154ed39e6e65e5b47a860cf8279184131a59de6e
1803119 <1803119@rgu.ac.uk>
2022-04-02 16:30:12 +0100

    Created initial basic server file + index page

---
+++ server.js
@@ -0,0 +1,17 @@
+// Load express and create app
+var express = require('express');
+var app = express();
+
+const PORT = process.env.PORT || 8080;
+
+// Set the port based on environment
+var port = PORT;
+
+// send index.html file as home page
+app.get('/', function(req, res){
+    res.sendFile(__dirname + 'index.html');
+});
+
+// start server
+app.listen(PORT);
+console.log('Express Server running');
