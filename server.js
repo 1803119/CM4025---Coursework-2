d8aab2f58cf564f49ce0b6761468f05802557f0a
1803119 <1803119@rgu.ac.uk>
2022-04-02 19:47:52 +0100

    Added console log of mongo uri

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ var port = PORT;
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = process.env.MONGO_URI;
+console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 client.connect(err => {
   const collection = client.db("test").collection("devices");
