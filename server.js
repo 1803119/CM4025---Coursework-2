7f40f40c33c26d4da180e9808742ef7d4a383904
1803119 <1803119@rgu.ac.uk>
2022-04-02 21:16:15 +0100

    Insert one record added after connection

--- server.js
+++ server.js
@@ -14,9 +14,15 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = process.env.MONGO_URI;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
+    console.log("Connected to MongoDB");
+    const collection = client.db("test").collection("devices");
+    // perform actions on the collection object
+    collection.insertOne({name: "Andrew", age: 21}, function(err, res){
+    if (err) throw err;
+        console.log("1 user inserted");
+    });
+    
+    client.close();
 });
 
 // const uri = process.env.MONGO_URI;
