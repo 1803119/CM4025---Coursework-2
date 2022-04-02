4d3e8e215365d3ee2b47bb946ca94bf59d4c18b8
1803119 <1803119@rgu.ac.uk>
2022-04-02 19:40:21 +0100

    Testing adding one record

--- server.js
+++ server.js
@@ -15,6 +15,11 @@ client.connect(err => {
   const collection = client.db("test").collection("devices");
   // perform actions on the collection object
   console.log("Mongo running");
+  collection.insertOne({name: "Andrew", age: 21}, function(err, res){
+      if (err) throw err;
+      console.log("1 user inserted");
+
+  })
   client.close();
 });
 
