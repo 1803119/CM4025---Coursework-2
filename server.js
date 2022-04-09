c6438109283c6829a62759e62f829966e5033ffe
1803119 <1803119@rgu.ac.uk>
2022-04-10 00:06:53 +0100

    Added public folder

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ const PORT = process.env.PORT || 8080;
 var port = PORT;
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = process.env.MONGO_URI;
