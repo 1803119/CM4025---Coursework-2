2e127b71e1de89f451263dfca61eb9af97b2e089
1803119 <1803119@rgu.ac.uk>
2022-04-11 23:28:56 +0100

    Testing

--- server.js
+++ server.js
@@ -99,9 +99,9 @@ app.route('/login')
     //var inputAge = req.body.inputAge;
     //console.log("The parmeters are Name: " + inputName + ", Age: " + inputAge);
     client.db().collection("users").findOne({emailAddress: data.emailAddress}, function(err, user){
-        if(err){
-            res.send("Login details do not match our records");
-        }
+        // if(err){
+        //     res.send("Login details do not match our records");
+        // }
         if(user == undefined || user == null){
             res.send("Login details do not match our records");
         }
@@ -109,9 +109,6 @@ app.route('/login')
         
         if(user != null){
             bcrypt.compare(data.password, user.password, function(err, success){
-                if(err){
-                    res.send("Error");
-                }
                 if(success == true){
 
 
