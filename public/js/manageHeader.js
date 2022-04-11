3f6456697ab392b0c8ce7f11db34e962ba15cb28
1803119 <1803119@rgu.ac.uk>
2022-04-11 21:42:12 +0100

    Added partials

---
+++ public/js/manageHeader.js
@@ -0,0 +1,27 @@
+
+function manageHeader(){
+    var res = "<%- firstName %>";
+    console.log(res);
+
+    if(res != "Not logged in"){
+        var initialButtons = document.getElementsByClassName("initialButtons");
+        
+        while(initialButtons.length > 0){
+            initialButtons[0].parentNode.removeChild(initialButtons[0]);
+        }
+
+        var greeting = document.getElementById("greeting");
+        greeting.innerText = "Hello, " + res;
+
+        var logoutLink = document.createElement("a");
+        logoutLink.appendChild(document.createTextNode("Logout"));
+
+        logoutLink.onclick = function(){
+            document.cookie = "token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+        }
+
+        logoutLink.href = "/";
+                    
+        document.getElementById("nav-li").appendChild(logoutLink);
+    }
+}
