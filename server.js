44f7f65d213c0c69ba38008bb54f95389470f313
1803119 <1803119@rgu.ac.uk>
2022-04-12 10:19:36 +0100

    Testing

--- server.js
+++ server.js
@@ -366,7 +366,14 @@ app.route('/shop')
     //var newItemStock = parseInt(data.itemStock) - parseInt(data.quantity);
     client.db().collection("users").findOne({emailAddress: payload.emailAddress, cart: {$elemMatch: {itemName: data.itemName}}}, function(err, result){
         if(result != null){
-            console.log(result.quantity);
+            var cart = result.cart
+            console.log(cart);
+            cart.forEach(item => {
+                if(item.itemName == data.itemName){
+                    item.quantity += data.quantity;
+                }
+            });
+            console.log(cart);
             client.db().collection("users").updateOne({emailAddress: payload.emailAddress, cart: {$elemMatch: {itemName: data.itemName}}}, {$set: {"cart.$" : {quantity: result.quantity + data.quantity}}}, function(err1, res1){
                 //res.redirect("/shop");
             });
